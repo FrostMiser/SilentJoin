92a8edda3e12fb8c3ee975599cb7019a8f8ce45c
FrostMiser <FrostMiser@users.noreply.github.com>
2017-01-17 22:26:18 -0600

    Cleanup.

--- src/com/frostcraft/silentjoin/SilentJoin.java
+++ src/com/frostcraft/silentjoin/SilentJoin.java
@@ -1,8 +1,5 @@
 package com.frostcraft.silentjoin;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.ChatColor;
 
